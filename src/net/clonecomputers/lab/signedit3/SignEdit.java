4406090e0a085723803cbc97787939a245d1f145
g_rocket <g.rocket1@gmail.com>
2014-05-25 19:03:15 -0400

    The plugin now properly disables itself

--- src/net/clonecomputers/lab/signedit3/SignEdit.java
+++ src/net/clonecomputers/lab/signedit3/SignEdit.java
@@ -1,16 +1,19 @@
 package net.clonecomputers.lab.signedit3;
 
+import org.bukkit.event.block.*;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.*;
 
 public class SignEdit extends JavaPlugin {
+	EditBeginListener editBeginListener = new EditBeginListener();
+	
 	public void onDisable() {
-		getLogger().info("SignEdit 3.0 doesn't know how to disable itself");
+		BlockPlaceEvent.getHandlerList().unregister(editBeginListener);
+		getLogger().info("SignEdit 3.0 Disabled");
 	}
 
 	public void onEnable() {
 		getLogger().info("SignEdit 3.0 Enabled");
-        PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(new EditBeginListener(), this);
+        this.getServer().getPluginManager().registerEvents(editBeginListener, this);
 	}
 }
