26782e987d7c600044f709379fd3c893d97687fc
g_rocket <g.rocket1@gmail.com>
2014-05-28 17:28:51 -0400

    checks to see if using patched CraftBukkit, warns and disables if not
    also fix version number

--- src/net/clonecomputers/lab/signedit3/SignEdit.java
+++ src/net/clonecomputers/lab/signedit3/SignEdit.java
@@ -1,5 +1,7 @@
 package net.clonecomputers.lab.signedit3;
 
+import org.bukkit.block.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.block.*;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.*;
@@ -9,11 +11,26 @@ public class SignEdit extends JavaPlugin {
 	
 	public void onDisable() {
 		BlockPlaceEvent.getHandlerList().unregister(editBeginListener);
-		getLogger().info("SignEdit 3.0 Disabled");
+		getLogger().info(String.format("%s %s Disabled", this.getDescription().getName(), this.getDescription().getVersion()));
 	}
 
 	public void onEnable() {
-		getLogger().info("SignEdit 3.0 Enabled");
-        this.getServer().getPluginManager().registerEvents(editBeginListener, this);
+		boolean patchApplied;
+		try {
+			HumanEntity.class.getMethod("openSign", Sign.class, boolean.class);
+			patchApplied = true;
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			patchApplied = false;
+		}
+		if(patchApplied) {
+			getLogger().info(String.format("%s %s Enabled", this.getDescription().getName(), this.getDescription().getVersion()));
+			this.getServer().getPluginManager().registerEvents(editBeginListener, this);
+		} else {
+			getLogger().severe("This version of CraftBukkit does not appear to have the sign patch applied");
+			getLogger().severe("SignEdit 3 needs this patch to be installed to work");
+			getPluginLoader().disablePlugin(this);
+		}
 	}
 }
